ae5b0336c7e21fe078b54898d31626bd36110afe
dhanur350 <dhanur297@gmail.com>
2023-02-05 23:36:16 +0530

    Day2.8

--- sagaredux/src/components/Redux/Store.jsx
+++ sagaredux/src/components/Redux/Store.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import ProductSaga from "./ProductSaga"
+import createSagaMiddleware from "redux-saga"
+import {configureStore} from "@reduxjs/toolkit"
+import RootReducer from "./RootReducer";
 
-function Store() {
-  return (
-    <div>Store</div>
+
+  //Creates a Redux middleware and connects the Sagas to the Redux Store
+  const sagaMiddleware = createSagaMiddleware();
+  const Store = configureStore(
+    {
+      reducer:RootReducer,
+      middleware:()=>[sagaMiddleware]
+    }
+    /**@desc middleware always registers in via callback and array format like this above  */
   )
-}
+  sagaMiddleware.run(ProductSaga)
 
+  /**@desc run() Dynamically run saga. Can be used to run Sagas only after the applyMiddleware phase. */
 export default Store
+
+/**@desc saga must be a function which returns a Generator Object. The middleware will then iterate over the Generator and execute all yielded Effects. */
