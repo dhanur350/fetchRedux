b172c617e20b85e8eb699da883e4e57ff0a25ee8
dhanur350 <dhanur297@gmail.com>
2023-02-06 23:55:50 +0530

    Day3

--- sagaredux/src/components/Redux/RootReducer.jsx
+++ sagaredux/src/components/Redux/RootReducer.jsx
@@ -1,7 +1,7 @@
-import {combineReducers} from 'redux'
-import { cartData } from './Reducer'
-import {productData} from './ProductReducer'
+import { combineReducers } from "redux";
+import { cartData } from "./Reducer";
+import { productData } from "./ProductReducer";
 export default combineReducers({
-    cartData,
-    productData
-})
+  cartData,
+  productData,
+});
